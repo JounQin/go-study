f9668797065eaf227bf1c3d63accc9ab15ffdd39
JounQin <admin@1stg.me>
2023-04-21 03:45:26 +0800

    feat: finish fuzz

---
+++ fuzz/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
+
+func Reverse(s string) (string, error) {
+	fmt.Printf("input: %q\n", s)
+	if utf8.ValidString(s) == false {
+		return s, errors.New("input is not valid UTF-8")
+	}
+	r := []rune(s)
+	fmt.Printf("runes: %q\n", r)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
+}
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
---
+++ fuzz/reverse_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func TestReverse(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{" ", " "},
+		{"!12345", "54321!"},
+	}
+	for _, tc := range testcases {
+		rev, err := Reverse(tc.in)
+		if err != nil {
+			t.Errorf("Reverse: %q, err: %v", tc.in, err)
+		}
+		if rev != tc.want {
+			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+		}
+	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, err := Reverse(orig)
+		if err != nil {
+			return
+		}
+		doubleRev, err := Reverse(rev)
+		if err != nil {
+			return
+		}
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d",
+			utf8.RuneCountInString(orig),
+			utf8.RuneCountInString(rev),
+			utf8.RuneCountInString(doubleRev),
+		)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
